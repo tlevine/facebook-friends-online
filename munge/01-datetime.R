cb14eb60f0711ad33ff3217740c1aa8e6890f2d0
Thomas Levine <occurrence@thomaslevine.com>
2012-09-16 10:38:23 -0400

    aoue

--- munge/01-datetime.R
+++ munge/01-datetime.R
@@ -1,1 +1,2 @@
+# As datetimes
 log.status$datetime <- as.POSIXct(log.status$ts, origin='1970-01-01')
--- src/stats.R
+++ src/stats.R
@@ -17,8 +17,21 @@ stats.time.on.fb <- function(
   end = as.POSIXct(Sys.Date()),
   ...
 ) {
-  data.subset <- stats._subset(data, uid, start, end)
-  data.subset
+
+  # Subset by user and time span, then pick out the interesting columns.
+  data <- stats._subset(data, uid, start, end)[c('datetime', 'status')]
+
+  # Previous datetime
+  # For the last one, the previous is the same as the current; this makes it
+  # easy to ignore that one while not messing up types.
+  data$datetime_next <- c(data$datetime[-1], tail(log.status$datetime, n=1))
+
+  # Duration of the status
+  data$interval <- interval(data$datetime, data$datetime_next)
+  data$duration <- as.duration(data$interval)
+
+  data <- subset(data, status == 'avail')
+  data
 }
 
 # stats.time.on.fb(log.status, 'xmpp:-1374361540@chat.facebook.com')
