8ecbfaa40d174bffa33abe14220e505dc74decd4
Thomas Levine <occurrence@thomaslevine.com>
2012-09-16 10:47:08 -0400

    yay

--- src/stats.R
+++ src/stats.R
@@ -2,36 +2,38 @@
 stats._subset <- function(
   data,
   the.uid,
-  start,
-  end
+  start = as.POSIXct('2012-01-01'),
+  end = as.POSIXct(Sys.Date())
 ) {
   # Inclusive at start datetime, exclusive at end datetime
   subset(data, uid == the.uid) # & datetime >= start & datetime < end)
 }
 
-# Time spent on Facebook by user within a time span
-stats.time.on.fb <- function(
-  data,
-  uid,
-  start = as.POSIXct('2012-01-01'),
-  end = as.POSIXct(Sys.Date()),
-  ...
-) {
-
-  # Subset by user and time span, then pick out the interesting columns.
-  data <- stats._subset(data, uid, start, end)[c('datetime', 'status')]
-
+# Time spent on Facebook by user within a time span, assuming that the
+# data include only one user
+stats.time.on.fb.for.one.user <- function(status) {
   # Previous datetime
   # For the last one, the previous is the same as the current; this makes it
   # easy to ignore that one while not messing up types.
-  data$datetime_next <- c(data$datetime[-1], tail(log.status$datetime, n=1))
+  status$datetime_next <- c(status$datetime[-1], tail(status$datetime, n=1))
 
   # Duration of the status
-  data$interval <- interval(data$datetime, data$datetime_next)
-  data$duration <- as.duration(data$interval)
+  status$interval <- interval(status$datetime, status$datetime_next)
+  status$duration <- as.duration(status$interval)
 
-  data <- subset(data, status == 'avail')
-  data
+  # Only statuses of "avail", indicating the start of onlineness
+  status.avail <- subset(status, status == 'avail')
+
+  # Dunno why I need to turn this back into seconds
+  total_duration <- dseconds(sum(status.avail$duration)) 
+
+  total_duration
 }
 
+# Subset by user and time span, then pick out the interesting columns.
+# status <- stats._subset(data, uid, start, end)[c('datetime', 'status')]
+# time.on.fb <- ddply(log.status, 'uid', stats.time.on.fb.for.one.user)      
+
+# Doesn't work
 # stats.time.on.fb(log.status, 'xmpp:-1374361540@chat.facebook.com')
+
